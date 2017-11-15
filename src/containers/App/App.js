d7ba60e306e26cf483aa9938aba4e676daec4f65
Eric Zhao <eric.zhao@carsales.com.au>
2017-11-15 16:03:55 +1100

    Add hamburger button

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,130 +1,122 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import Menu from './../Menu/Menu';
 import { IndexLink } from 'react-router';
-import { LinkContainer } from 'react-router-bootstrap';
-import Navbar from 'react-bootstrap/lib/Navbar';
-import Nav from 'react-bootstrap/lib/Nav';
-import NavItem from 'react-bootstrap/lib/NavItem';
-import Helmet from 'react-helmet';
-import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
-import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
-import { InfoBar } from 'components';
-import { push } from 'react-router-redux';
-import config from '../../config';
-import { asyncConnect } from 'redux-async-connect';
+// import { slide as Menu } from 'react-burger-menu';
+// import { connect } from 'react-redux';
+// import { IndexLink } from 'react-router';
+// import { LinkContainer } from 'react-router-bootstrap';
+// import Navbar from 'react-bootstrap/lib/Navbar';
+// import Nav from 'react-bootstrap/lib/Nav';
+// import NavItem from 'react-bootstrap/lib/NavItem';
+// import Helmet from 'react-helmet';
+// import { InfoBar } from 'components';
+// import { push } from 'react-router-redux';
+// import config from '../../config';
+// import { asyncConnect } from 'redux-async-connect';
 
-@asyncConnect([{
-  promise: ({store: {dispatch, getState}}) => {
-    const promises = [];
+// @asyncConnect([{
+// promise: ({store: {dispatch, getState}}) => {
+// promises.push(dispatch(loadInfo()));
 
-    if (!isInfoLoaded(getState())) {
-      promises.push(dispatch(loadInfo()));
-    }
-    if (!isAuthLoaded(getState())) {
-      promises.push(dispatch(loadAuth()));
-    }
-
-    return Promise.all(promises);
+// return Promise.all(promises);
+// }
+// }])
+// @connect(
+//   state => ({user: state.auth.user}),
+//   {logout, pushState: push})
+const menuStyles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
   }
-}])
-@connect(
-  state => ({user: state.auth.user}),
-  {logout, pushState: push})
+};
+
+const menuItemStyle = {
+  display: 'block',
+  outline: 'none',
+  marginLeft: '10px',
+  fontWeight: '700',
+  fontFamily: '"Raleway", Arial, sans-serif',
+  color: '#b8b7ad',
+  fontSize: '1.15em'
+};
+
 export default class App extends Component {
+
+
   static propTypes = {
     children: PropTypes.object.isRequired,
-    user: PropTypes.object,
-    logout: PropTypes.func.isRequired,
-    pushState: PropTypes.func.isRequired
+    // user: PropTypes.object,
+    // logout: PropTypes.func.isRequired,
+    // pushState: PropTypes.func.isRequired
   };
 
-  static contextTypes = {
-    store: PropTypes.object.isRequired
-  };
-
-  componentWillReceiveProps(nextProps) {
-    if (!this.props.user && nextProps.user) {
-      // login
-      this.props.pushState('/loginSuccess');
-    } else if (this.props.user && !nextProps.user) {
-      // logout
-      this.props.pushState('/');
-    }
+  constructor() {
+    super();
+    this.state = { menuOpen: false };
   }
 
-  handleLogout = (event) => {
-    event.preventDefault();
-    this.props.logout();
-  };
+  // static contextTypes = {
+  // store: PropTypes.object.isRequired
+  // };
+
 
   render() {
-    const {user} = this.props;
     const styles = require('./App.scss');
 
     return (
-      <div className={styles.app}>
-        <Helmet {...config.app.head}/>
-        <Navbar fixedTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
-                <div className={styles.brand}/>
-                <span>{config.app.title}</span>
-              </IndexLink>
-            </Navbar.Brand>
-            <Navbar.Toggle/>
-          </Navbar.Header>
-
-          <Navbar.Collapse eventKey={0}>
-            <Nav navbar>
-              {user && <LinkContainer to="/chat">
-                <NavItem eventKey={1}>Chat</NavItem>
-              </LinkContainer>}
-
-              <LinkContainer to="/widgets">
-                <NavItem eventKey={2}>Widgets</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/survey">
-                <NavItem eventKey={3}>Survey</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/pagination">
-                <NavItem eventKey={4}>Pagination</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/about">
-                <NavItem eventKey={5}>About Us</NavItem>
-              </LinkContainer>
-
-              {!user &&
-              <LinkContainer to="/login">
-                <NavItem eventKey={6}>Login</NavItem>
-              </LinkContainer>}
-              {user &&
-              <LinkContainer to="/logout">
-                <NavItem eventKey={7} className="logout-link" onClick={this.handleLogout}>
-                  Logout
-                </NavItem>
-              </LinkContainer>}
-            </Nav>
-            {user &&
-            <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
-            <Nav navbar pullRight>
-              <NavItem eventKey={1} target="_blank" title="View on Github" href="https://github.com/erikras/react-redux-universal-hot-example">
-                <i className="fa fa-github"/>
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
-        <div className={styles.appContent}>
+      <div className={styles.app} >
+        <div id="outer-container" >
+          <Menu styles={menuStyles} isOpen={this.menuOpen}>
+            <IndexLink to="/" className="menu-item" style={menuItemStyle} activeStyle={{ color: '#33e0ff' }} >
+              <div className="menu-item" />
+            </IndexLink>
+            <IndexLink to="/leaderboard" className="menu-item" style={menuItemStyle} activeStyle={{ color: '#33e0ff' }}>
+              <div className="menu-item" />
+              <span >Leader Board</span>
+            </IndexLink>
+            <IndexLink to="/about" className="menu-item" style={menuItemStyle} activeStyle={{ color: '#33e0ff' }}>
+              <div className="menu-item" />
+              <span >About</span>
+            </IndexLink>
+          </Menu>
+        </div>
+        <div>
           {this.props.children}
         </div>
-        <InfoBar/>
-
         <div className="well text-center">
           Have questions? Ask for help <a
-          href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-          target="_blank">on Github</a> or in the <a
-          href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
+            href="https://github.com/erikras/react-redux-universal-hot-example/issues"
+            target="_blank">on Github</a> or in the <a
+              href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
         </div>
       </div>
     );
