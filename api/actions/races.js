ba23e9c7046d590a5637db89a64b1f59e686ceb2
Eric Zhao <eric.zhao@carsales.com.au>
2017-11-17 10:57:14 +1100

    logi cupdate

--- api/actions/races.js
+++ api/actions/races.js
@@ -5,25 +5,30 @@ async function races() {
     const db = Admin.firestore();
     const ret = [];
     const snapshot = await db.collection('races').get();
-    for (let index = 0; index < snapshot.docs.length; index++) {
-        const doc = snapshot.docs.sort(s=>s.date.date).reverse()[index];
+    const temp  = snapshot.docs.map(s =>
+        ({
+            id: s.id,
+            race: s.data()
+        }));
+    const docs = temp.sort(s => s.race.date).reverse();
+    for (let index = 0; index < docs.length; index++) {
+        const doc = docs[index];
+        const data = doc.race;
         // MMM d
-        const data = doc.data();
-        var date =new Date(data.date);
         const race = {
-            raceId: doc.id, 
-            city: data.country, 
+            raceId: doc.id,
+            city: data.country,
             country: data.state,
-            state: data.city, 
-            date: moment(date, 'MMM dd, hh:mm'),
-            venue: data.venue, 
-            team1: data.team1, 
+            state: data.city,
+            date: moment.default(new Date(data.date)).format('MMM ddd, hh:mm'),
+            venue: data.venue,
+            team1: data.team1,
             team2: data.team2
         };
 
         race.team1Result = await getResult(race.team1, doc.id);
         race.team2Result = await getResult(race.team2, doc.id);
-        
+
         ret.push(race);
     }
     return ret;
@@ -43,13 +48,13 @@ async function getResult(name, raceId) {
     const data = doc.data();
 
     const teamResult = {
-        team: data.team, 
-        raceTime: data.race_time, 
+        team: data.team,
+        raceTime: data.race_time,
         reactionTime: data.reaction_time,
-        splitTime: data.split_time, 
+        splitTime: data.split_time,
         combinedTime: data.combined_time
     };
-    
+
     return teamResult;
 }
 
