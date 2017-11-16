918d2a0f4fbfa8b8441e39d82ac224a4adf6f0cf
Eric Zhao <eric.zhao@carsales.com.au>
2017-11-16 15:19:20 +1100

    detail api

--- api/actions/index.js
+++ api/actions/index.js
@@ -2,5 +2,6 @@ export loadInfo from './loadInfo';
 export loadAuth from './loadAuth';
 export login from './login';
 export logout from './logout';
+export {getRaces, getRace} from './race';
 export * as widget from './widget/index';
 export * as survey from './survey/index';
---
+++ src/api/racesApi.js
@@ -0,0 +1,9 @@
+async function getRace(eventId){
+    return await fetch('api/')
+}
+
+async function getRaces(){
+
+};
+
+export {getRace, getRaces};
